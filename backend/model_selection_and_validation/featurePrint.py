0a3b90c1bf85b8ab090db4ddb268cff7792fd212
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-12-30 14:08:15 +0530

    including file path changed

--- backend/model_selection_and_validation/featurePrint.py
+++ backend/model_selection_and_validation/featurePrint.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-th = pd.read_csv('C:\\Users\\user\\SreelakshmiK\\personal\\Projects\\She-Health\\backend\\data\\cleaned_dataset_Thyroid1.csv')
-cer = pd.read_csv('C:\\Users\\user\\SreelakshmiK\\personal\\Projects\\She-Health\\backend\\data\\cervical-cancer_csv.csv')
-pc = pd.read_csv('C:\\Users\\user\\SreelakshmiK\\personal\\Projects\\She-Health\\backend\\data\\PCOS_data.csv')
-en = pd.read_csv('C:\\Users\\user\\SreelakshmiK\\personal\\Projects\\She-Health\\backend\\data\\structured_endometriosis_data.csv')
+th = pd.read_csv('C:\\Users\\user\\SreelakshmiK\\personal\\Projects\\She-Health\\backend\\data\\set_1\\cleaned_dataset_Thyroid1.csv')
+cer = pd.read_csv('C:\\Users\\user\\SreelakshmiK\\personal\\Projects\\She-Health\\backend\\data\\set_1\\cervical-cancer_csv.csv')
+pc = pd.read_csv('C:\\Users\\user\\SreelakshmiK\\personal\\Projects\\She-Health\\backend\\data\\set_1\\PCOS_data.csv')
+en = pd.read_csv('C:\\Users\\user\\SreelakshmiK\\personal\\Projects\\She-Health\\backend\\data\\set_1\\structured_endometriosis_data.csv')
 
 data = {
     'Thyroid': th.columns.tolist(),
